9d3b75196ad11fccf3eab8a4a4a8ebfeec508a66
Andrey Belyaev <ianbel263@gmail.com>
2019-12-02 09:55:25 +0300

    Заменяет "рыбные" данные на МОКИ

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import {createBoardTemplate} from './components/board.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
 import {createTaskTemplate} from './components/task.js';
 import {createLoadMoreButtomTemplate} from './components/load-more-btn.js';
+import {generateAllTasks} from './mock/task.js';
+import {generateFilter} from './mock/filter.js';
 
-const TASK_NUMBER = 3;
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -14,15 +18,18 @@ const render = (container, template, place = `beforeend`) => {
 const main = document.querySelector(`.main`);
 const siteControl = main.querySelector(`.main__control`);
 render(siteControl, createSiteMenuTemplate());
-render(main, createSiteFilterTemplate());
+
+const filters = generateFilter();
+render(main, createSiteFilterTemplate(filters));
+
 render(main, createBoardTemplate());
 
 const taskList = main.querySelector(`.board__tasks`);
-render(taskList, createTaskEditTemplate());
+const tasks = generateAllTasks(TASK_COUNT);
 
-for (let i = 1; i <= TASK_NUMBER; i++) {
-  render(taskList, createTaskTemplate());
-}
+render(taskList, createTaskEditTemplate(tasks[0]));
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+tasks.slice(1, showingTasksCount).forEach((task) => render(taskList, createTaskTemplate(task)));
 
 const board = main.querySelector(`.board`);
 render(board, createLoadMoreButtomTemplate());
